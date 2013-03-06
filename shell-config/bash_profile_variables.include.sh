eb339cfee48edbf2d69dee6ccba545920fdedf3c
Tor Håvard Bieltvedt <th@bieltvedt.net>
2013-03-07 00:27:31 +0100

    Fix environment variables for Mac.

--- shell-config/bash_profile_variables.include.sh
+++ shell-config/bash_profile_variables.include.sh
@@ -1,7 +1,7 @@
 ### Base directories ###
-DEV_HOME=~/Development
-DEV_SOFTWARE_HOME=/cygdrive/c/Dev-Software
-GIT_REPOSITORIES_HOME=$DEV_HOME/git-repos
+DEV_HOME=~/Dev
+DEV_SOFTWARE_HOME=${DEV_HOME}/Software
+GIT_REPOSITORIES_HOME=${DEV_HOME}/Git-Repositories
 
 ### Home directories export ###
 export JAVA_HOME=$DEV_SOFTWARE_HOME/Java/jdk1.6.0_38
@@ -10,4 +10,4 @@ export MVN_HOME=$DEV_SOFTWARE_HOME/Maven/apache-maven-3.0.4
 export PLAY_2_0_HOME=${DEV_SOFTWARE_HOME}/Play/play-2.0.2
 export PLAY_2_1_HOME=${DEV_SOFTWARE_HOME}/Play/play-2.1.0
 export PLAY_HOME=${PLAY_2_1_HOME}
-export JREBEL_HOME=C:\\Dev-Software-Tine\\JRebel\\jrebel-5.1.2
+export JREBEL_HOME=$DEV_SOFTWARE_HOME/JRebel/jrebel-5.1.2
