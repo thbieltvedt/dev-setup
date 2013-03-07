9c3710c88293699483ca5c201ae41770ab90d5d3
Tor Håvard Bieltvedt <th@bieltvedt.net>
2013-03-07 01:36:49 +0100

    Renamed Dev directory to Development.

--- shell-config/bash_profile_variables.include.sh
+++ shell-config/bash_profile_variables.include.sh
@@ -1,11 +1,11 @@
 ### Base directories ###
-DEV_HOME=~/Dev
-DEV_SOFTWARE_HOME=${DEV_HOME}/Software
+             DEV_HOME=~/Development
+    DEV_SOFTWARE_HOME=${DEV_HOME}/Software
 GIT_REPOSITORIES_HOME=${DEV_HOME}/Git-Repositories
 
 ### Home directories export ###
-export JAVA_HOME=$DEV_SOFTWARE_HOME/Java/jdk1.6.0_38
-export GIT_HOME=$DEV_SOFTWARE_HOME/Git/Git-1.7.11
-export MVN_HOME=$DEV_SOFTWARE_HOME/Maven/apache-maven-3.0.4
-export PLAY_HOME=${DEV_SOFTWARE_HOME}/Play/play-2.1.0
+export   JAVA_HOME=$DEV_SOFTWARE_HOME/Java/jdk1.6.0_38
+export    GIT_HOME=$DEV_SOFTWARE_HOME/Git/Git-1.7.11
+export    MVN_HOME=$DEV_SOFTWARE_HOME/Maven/apache-maven-3.0.4
+export   PLAY_HOME=${DEV_SOFTWARE_HOME}/Play/play-2.1.0
 export JREBEL_HOME=$DEV_SOFTWARE_HOME/JRebel/jrebel-5.1.2
